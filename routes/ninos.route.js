0152c2041d5cacc4688a2f3d678f484f438e9a20
jesus.ropero.b <jesus.ropero.b@outlook.com>
2024-09-25 21:54:00 -0500

    cambios sql

--- routes/ninos.route.js
+++ routes/ninos.route.js
@@ -1,15 +1,14 @@
 import { Router } from "express";
-import auth from "../middleware/auth.js";
+//import auth from '../middleware/auth.js';
 import { deleteNino, getAllNinos, getNino, setNino, updateNino } from "../models/nino.models.js";
-
 
 const router = Router();
 
-router.get("/api/nino/",  auth, getAllNinos);
-router.get("/api/nino/:id", auth,getNino);
-router.post("/api/nino/", auth,setNino);
-router.put("/api/nino/:id", auth,updateNino);
-router.delete("/api/nino/:id", auth,deleteNino);
+router.get("/api/nino/",  getAllNinos);
+router.get("/api/nino/:id", getNino);
+router.post("/api/nino/", setNino);
+router.put("/api/nino/:id",  updateNino);
+router.delete("/api/nino/:id",  deleteNino);
 
 
 
