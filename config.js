c63af83a9aaea66a4c1032ea9f523f1ea840299e
Jesus Ropero <63603819+rbchus@users.noreply.github.com>
2024-11-10 00:13:24 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 config()
 export const PORT = process.env.PORT || 3001;
-export const DB_HOST = process.env.DB_HOST || "us-bos-web1716.main-hosting.eu";
+export const DB_HOST = process.env.DB_HOST || "127.0.0.1";
 export const DB_USER = process.env.DB_USER || "u218113810_OcanaPark";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "OcanaP4rk&24";
 export const DB_DATABASE = process.env.DB_DATABASE || "u218113810_OcanaPark";
