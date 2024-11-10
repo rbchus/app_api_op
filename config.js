ce6225da565b3f9e0ffe4e4868de0677f2e671d5
ManuelRoperoM <142428701+ManuelRoperoM@users.noreply.github.com>
2024-11-09 23:04:47 -0500

    Update database config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 import { config } from "dotenv";
 config()
 export const PORT = process.env.PORT || 3001;
-export const DB_HOST = process.env.DB_HOST || "junction.proxy.rlwy.net";
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "UmWrXOdNqzelABEXlKJenWxspbAebEUA";
-export const DB_DATABASE = process.env.DB_DATABASE || "railway";
-export const DB_PORT = process.env.DB_PORT || 14030;
+export const DB_HOST = process.env.DB_HOST || "us-bos-web1716.main-hosting.eu";
+export const DB_USER = process.env.DB_USER || "u218113810_OcanaPark";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "OcanaP4rk&24";
+export const DB_DATABASE = process.env.DB_DATABASE || "u218113810_OcanaPark";
+export const DB_PORT = process.env.DB_PORT || 3306;
 export const SECRET_KEY = process.env.SECRET_KEY || "papichulo";
 
 /*
