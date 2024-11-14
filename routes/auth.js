eaa441152bfca1f8be9ab8dd373e39cf39055d13
Carlos <crestrepor_ext@obtenmas.com>
2024-11-14 10:28:31 -0500

    Mensaje del servidor

--- routes/auth.js
+++ routes/auth.js
@@ -1,54 +1,47 @@
 import { Router } from "express";
 import { pool } from "../db.js";
+import jwt from 'jsonwebtoken';
+import { SECRET_KEY } from "../config.js";
+
 const router = Router();
 
-import jwt from 'jsonwebtoken';
-
-
-import {
-  SECRET_KEY,
-} from "../config.js";
-
-
-
-
-router.post("/", async ( req, res) => {
+router.post("/", async (req, res) => {
   try {
     const correo_usuario = req.body.correo_usuario;
-    const clave_usuario  = req.body.clave_usuario;
+    const clave_usuario = req.body.clave_usuario;
     
+    // Intentar una consulta para verificar la conexión a la base de datos
     const [rows] = await pool.query(
       "SELECT * FROM usuarios_x_rol WHERE correo_usuario = ? AND clave_usuario = ?",
-      [correo_usuario , clave_usuario]
+      [correo_usuario, clave_usuario]
     );
 
     if (rows.length > 0) {
-
       const token = jwt.sign({ id: correo_usuario }, SECRET_KEY, {
         expiresIn: 86400 // 24 horas
-    });
+      });
 
-      return res.status(200).json(
-        { auth: true ,
-          message: "Usuario logeado con existo" ,
-          data:token
-        });
-    } else  {
-      return res.status(401).json(
-        { status: false ,
-          message: "Error de usuario o contraseña" ,
-          data:[]
-        });
+      return res.status(200).json({
+        auth: true,
+        message: "Usuario logeado con éxito",
+        data: token
+      });
+    } else {
+      return res.status(401).json({
+        status: false,
+        message: "Error de usuario o contraseña",
+        data: []
+      });
     }
-    
+
   } catch (error) {
-    return res.status(500).json({ message: "Error en la consulta" + error});
+    // Detectar si el error es de conexión a la base de datos
+    if (error.code === 'ECONNREFUSED') {
+      return res.status(500).json({ message: "Error: No se pudo conectar a la base de datos" });
+    } else {
+      return res.status(500).json({ message: "Error en la consulta: " + error.message });
+    }
   }
-
-
-
-})
-
-// Exports
+});
 
 export default router;
