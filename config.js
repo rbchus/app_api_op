6afb17407a2db6192057e5935662d4ed3ea92cb5
Carlos <crestrepor_ext@obtenmas.com>
2024-11-13 22:42:27 -0500

    Changue credentials

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 import { config } from "dotenv";
 config()
 export const PORT = process.env.PORT || 3001;
-export const DB_HOST = process.env.DB_HOST || "127.0.0.1";
-export const DB_USER = process.env.DB_USER || "u218113810_OcanaPark";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "OcanaP4rk&24";
-export const DB_DATABASE = process.env.DB_DATABASE || "u218113810_OcanaPark";
+export const DB_HOST = process.env.DB_HOST || "82.197.82.107";
+export const DB_USER = process.env.DB_USER || "u218113810_root";
+export const DB_PASSWORD = process.env.DB_PASSWORD || ">T2[gO~ifh";
+export const DB_DATABASE = process.env.DB_DATABASE || "u218113810_ocanapark_db";
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const SECRET_KEY = process.env.SECRET_KEY || "papichulo";
 
